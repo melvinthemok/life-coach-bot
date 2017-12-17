855ee6e9cfb6f6097686305fbe3e840174f2f490
melvinthemok <melvinmok.cy@gmail.com>
2017-12-17 23:46:51 +0800

    Add hard-coded responses

---
+++ responses.js
@@ -0,0 +1,48 @@
+const GREETINGS = [
+  'Hey beautiful',
+  'How you doin\'',
+  'What\'s up',
+  'Hi',
+  'Hey'
+]
+
+const SWEET_NOTHINGS = [
+  'You can do anything you put your mind to',
+  'You are perfect in every way',
+  'You deserve every happiness',
+  'The world is your oyster',
+  'No one can do what you can',
+  'Nothing can stop you',
+  'You can run through walls',
+  'You can achieve anything you want',
+  'You are special',
+  'You are exquisite'
+]
+
+const ASSERTIONS = [
+  '#truth',
+  'You know it\'s true',
+  'I mean it',
+  'I don\'t say anything I don\'t mean',
+  'Anyone who suggests otherwise is a fool',
+  'I can\'t be the first one to tell you that',
+  'Don\'t let anyone tell you otherwise',
+  'Ask anyone',
+  'If it were a lie, you\'d see through it',
+  'Whether you believe it or not, you\'re right'
+]
+
+const FAREWELLS = [
+  'Till next time',
+  'À bientôt',
+  'Later',
+  'Auf wiedersehen',
+  'Ciao bella'
+]
+
+module.exports = {
+  GREETINGS,
+  SWEET_NOTHINGS,
+  ASSERTIONS,
+  FAREWELLS
+}
