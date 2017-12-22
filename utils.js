64c79df99e4ecdaef3a864da0ad33f451b855ffe
melvinthemok <melvinmok.cy@gmail.com>
2017-12-23 01:29:43 +0800

    Add speakeasy-nlp and determineEmotionOf

--- utils.js
+++ utils.js
@@ -1,3 +1,27 @@
+const speakeasy = require('speakeasy-nlp')
+
+const { POSITIVE, NEGATIVE, CONFUSED, NEUTRAL } = require('./responses')
+
+const determineEmotionOf = (answer) => {
+
+  const positivity = speakeasy.sentiment.positivity(answer).score
+  const negativity = speakeasy.sentiment.negativity(answer).score
+
+  switch (true) {
+    case positivity > 0 && negativity === 0:
+      return POSITIVE
+      break
+    case positivity === 0 && negativity > 0:
+      return NEGATIVE
+      break
+    case positivity > 0 && negativity > 0:
+      return CONFUSED
+      break
+    default:
+      return NEUTRAL
+  }
+}
+
 const randomlySelectOneOfThe = (arr) => arr[Math.floor(Math.random()*arr.length)]
 
-module.exports = { randomlySelectOneOfThe }
+module.exports = { determineEmotionOf, randomlySelectOneOfThe }
